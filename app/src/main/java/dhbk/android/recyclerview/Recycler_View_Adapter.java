dd7608742dd0e444b793d9dc1ab806b445ccb2bc
Phong <p7157568@gmail.com>
2016-04-26 16:10:12 +0700

    feat - make viewholder bound when scroll

--- app/src/main/java/dhbk/android/recyclerview/Recycler_View_Adapter.java
+++ app/src/main/java/dhbk/android/recyclerview/Recycler_View_Adapter.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import java.util.Collections;
 import java.util.List;
@@ -40,7 +42,8 @@ public class Recycler_View_Adapter extends RecyclerView.Adapter<View_Holder>{
         holder.description.setText(list.get(position).description);
         holder.imageView.setImageResource(list.get(position).imageId);
 
-        //animate(holder);
+        // anim
+        animate(holder);
     }
 
     //returns the number of elements the RecyclerView will display
@@ -61,6 +64,11 @@ public class Recycler_View_Adapter extends RecyclerView.Adapter<View_Holder>{
         int position = list.indexOf(data);
         list.remove(position);
         notifyItemRemoved(position);
+    }
+
+    public void animate(RecyclerView.ViewHolder viewHolder) {
+        final Animation animAnticipateOvershoot = AnimationUtils.loadAnimation(context, R.anim.bounce_interpolator);
+        viewHolder.itemView.setAnimation(animAnticipateOvershoot);
     }
 
 }
